8288647d1f291ff259caafc0bdbf6b6bd33e1865
Chen Mu <3331400783@qq.com>
2023-11-14 21:04:19 +0800

    选择排序

---
+++ Alogrithm/Sequence table/selectSort.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+void selectSort(int a[], int n)
+{
+    for(int i = 0; i < n; ++i)
+    {
+        int min = i;
+        for(int j = i; j < n; ++j) 
+        {
+            if(a[j] < a[min])   min = j;
+        }
+        std::swap(a[i], a[min]);
+    }
+}
+
+int main()
+{
+    int a[10] = {10, 6, 7, 8, 5, 1, 3, 2, 4, 9};
+    int n = sizeof(a)/ sizeof(int);
+    selectSort(a, n);
+    for(int i = 0; i < n; ++i)  std::cout << a[i] << " ";
+    system("pause");
+    return 0;
+}
