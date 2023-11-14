f6d2ab488f279504b06aab481a4bf401dc5aac9e
Chen Mu <3331400783@qq.com>
2023-11-14 21:04:32 +0800

    插入排序

---
+++ Alogrithm/Sequence table/insertSort.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+void insertSort(int a[], int n)
+{
+    for(int i = 1; i < n; ++i)
+    {
+        for(int j = i; a[j - 1] > a[j] && j > 0; --j) 
+        {
+            std::swap(a[j - 1], a[j]);
+        }
+    }
+}
+
+int main()
+{
+    int a[10] = {10, 6, 7, 8, 5, 1, 3, 2, 4, 9};
+    int n = sizeof(a)/ sizeof(int);
+    insertSort(a, n);
+    for(int i = 0; i < n; ++i)  std::cout << a[i] << " ";
+    system("pause");
+    return 0;
+}
