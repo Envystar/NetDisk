ec332cd13c5c01f255119aeeaa2d499f71f246c2
Chen Mu <3331400783@qq.com>
2023-11-30 13:59:23 +0800

    双向链表

--- DataStructure/LinkedList/doublyLinkedList.cpp
+++ DataStructure/LinkedList/doublyLinkedList.cpp
@@ -10,60 +10,94 @@ public:
 	ListNode* right;
 };
 
-//添加到链表前端
-void pushLinkedListFront(ListNode* &leftHead,int val)
+class DoubleLinkedList
 {
-	if(leftHead->left != nullptr) return;
-	ListNode* newLeftHead = new ListNode(val, nullptr, leftHead);
-	leftHead->left = newLeftHead;
-	leftHead = newLeftHead;
-}
-
-//添加到链表右端
-void pushLinkedListBack(ListNode* &rightHead,int val)
-{
-	if(rightHead->right != nullptr) return;
-	ListNode* newRightHead = new ListNode(val, rightHead, nullptr);
-	rightHead->right = newRightHead;
-	rightHead = newRightHead;
-}
-
-//正向遍历输出
-void printFromFront(ListNode* leftHead)
-{
-	if(leftHead->left != nullptr) return;
-	while(leftHead != nullptr)
+public:
+	DoubleLinkedList()
 	{
-		std::cout<< leftHead->val << " ";
-		leftHead = leftHead->right;
+		this->m_LeftHead = new ListNode;
+		this->m_RightHead = new ListNode;
+		this->m_LeftHead->left = this->m_RightHead;
+		this->m_LeftHead->right = this->m_RightHead;
+		this->m_RightHead->left = this->m_LeftHead;
+		this->m_RightHead->right = this->m_LeftHead;
+		this->m_NodeNum = -1;
 	}
-	std::cout << std::endl;
-}
 
-//反向遍历输出
-void printFromBack(ListNode* rightHead)
-{
-	if(rightHead->right != nullptr) return;
-	while(rightHead != nullptr)
+	//重载运算符[]
+	int& operator[](int index)
 	{
-		std::cout<< rightHead->val << " ";
-		rightHead = rightHead->left;
+		ListNode* temp = this->m_LeftHead; 
+		for(int i = 0; i <= index; ++i)
+		{
+			temp = temp->right;
+		}
+		return temp->val;
 	}
-	std::cout << std::endl;
-}
+
+	//添加到链表前端
+	void pushLinkedListFront(int val)
+	{
+		ListNode* newLeftHead = new ListNode(val, this->m_LeftHead, this->m_LeftHead->right);
+		this->m_LeftHead->right->left = newLeftHead;
+		this->m_LeftHead->right = newLeftHead;
+		this->m_NodeNum++;
+	}
+
+	//添加到链表右端
+	void pushLinkedListBack(int val)
+	{
+		ListNode* newRightHead = new ListNode(val, this->m_RightHead->left, this->m_RightHead);
+		this->m_RightHead->left->right = newRightHead;
+		this->m_RightHead->left = newRightHead;
+		this->m_NodeNum++;
+	}
+
+	//正向遍历输出
+	void printFromFront()
+	{
+		ListNode* temp = this->m_LeftHead->right; 
+		for(int i = 0; i <= this->m_NodeNum; ++i)
+		{
+			std::cout<< temp->val << " ";
+			temp = temp->right;
+		}
+		std::cout << std::endl;
+	}
+
+	//反向遍历输出
+	void printFromBack()
+	{
+		ListNode* temp = this->m_RightHead->left; 
+		for(int i = 0; i <= this->m_NodeNum; ++i)
+		{
+			std::cout<< temp->val << " ";
+			temp = temp->left;
+		}
+		std::cout << std::endl;
+	}
+
+private:
+	ListNode* m_LeftHead;
+	ListNode* m_RightHead;
+	int m_NodeNum;
+};
+
+
 
 int main()
 { 
-    ListNode* leftNode = new ListNode(0);
-	ListNode* rightNode = new ListNode(1);
-	leftNode->right = rightNode;
-	rightNode->left = leftNode;
+    DoubleLinkedList linkedlist;
+	linkedlist.pushLinkedListFront(100);
+	linkedlist[0] = 50;
+	linkedlist.pushLinkedListFront(400);
+	linkedlist.printFromFront();
+	linkedlist.printFromBack();
+	linkedlist.pushLinkedListFront(200);
+	linkedlist.pushLinkedListBack(300);
+	linkedlist.printFromFront();
+	linkedlist.printFromBack();
 	
-	printFromBack(leftNode);
-	printFromFront(leftNode);
-	pushLinkedListFront(leftNode,10);
-	printFromFront(leftNode);
-	printFromBack(rightNode);
 	system("pause");
 	return 0;	
 }
