b8a4b52220157807c6187019dcf0e3cf12bfbbe1
Chen Mu <3331400783@qq.com>
2023-12-02 11:15:56 +0800

    快速排序

--- Alogrithm/SequenceTable/Sort/quickSort.cpp
+++ Alogrithm/SequenceTable/Sort/quickSort.cpp
@@ -3,16 +3,13 @@
 //快速排序(参数：数组最小和最大下标，数组本身)
 void quickSort(int left, int right, int a[])
 {
-    if(left >= right) return;
-    else
+    if(left < right)
     {
         int low = left, pivot = right;//选定最右侧元素
-        int(fast = left; i < pivote; ++fast)
+        for(int fast = left; fast < pivot; ++fast)
         {
             if(a[fast] < a[pivot])//找到比pivot指向小的元素
-            {
                 std::swap(a[fast], a[low++]);
-            }
         }
         std::swap(a[low], a[pivot]);//对换
         pivot = low;
@@ -27,6 +24,5 @@ int main()
     int n = sizeof(a)/ sizeof(int);
     quickSort(0, n - 1, a);
     for(int i = 0; i < n; ++i)  std::cout << a[i] << " ";
-    system("pause");
     return 0;
 }
