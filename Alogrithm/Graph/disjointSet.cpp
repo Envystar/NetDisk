68b41b34228c8151c0ce3b3653482c49cf855b3d
Chen Mu <3331400783@qq.com>
2024-02-22 20:20:07 +0800

    并查集

--- Alogrithm/Graph/disjointSet.cpp
+++ Alogrithm/Graph/disjointSet.cpp
@@ -1,68 +1,43 @@
 #include <bits/stdc++.h>
-#define VERTICES 6//顶点数 
-
-//并查集
-
-//初始化数组
-void initialise(int parent[]){
-    for(int i = 0; i < VERTICES; ++i){
-        parent[i] = -1;
-    }
-}
-
-//寻找根节点
-int findRoot(int x, int parent[]){
-    while(parent[x] >= 0)
-    {
-        x = parent[x];
-    }
-    return x;
-}
-
-//合并集合
-int unionVertices(int x, int y, int parent[])
-{
-    int xRoot = findRoot(x, parent);
-    int yRoot = findRoot(y, parent);
-    if(xRoot != yRoot){
-        if(parent[xRoot] <= parent[yRoot]){
-            if(parent[xRoot] == parent[yRoot])
-            {
-                --parent[xRoot];
-            }
-            parent[yRoot] = xRoot;
-        }
-        if(parent[xRoot] > parent[yRoot]){
-            parent[xRoot] = yRoot;
-        }
-        
-        return 1;
-    }
-    else{
-        return 0;
-    }
-}
 
 int main()
 {
-    int parent[VERTICES] = {0};
-    int edges[6][2] = {
-        {0, 1}, {5, 4}, {1, 3},
-        {1, 2}, {3, 4}, {5, 2},
+   int n, m;
+    std::cin >> n >> m;
+    std::vector<int> v(n, -1);
+    auto find = [&](auto self, int x) ->int {//寻找根节点
+        if(v[x] == -1) {
+            return x;
+        }
+        v[x] = self(self, v[x]);
+        return v[x];
     };
-    initialise(parent);
-    for(int i = 0; i < 6; ++i){
-        int x = edges[i][0];
-        int y = edges[i][1];
-        if(!unionVertices(x, y, parent)){
-            std::cout << "Cycle dectected!\n";
-            return 0;
+    auto uniset = [&](int x, int y) -> void {//合并集合
+        int px = find(find, x);
+        int py = find(find, y);
+        if(px != py) {
+            v[px] = py;
         }
-        for(int j = 0; j < VERTICES; ++j){
-            std::cout << parent[j] << ' ';
+    };
+    auto query = [&](int x, int y) ->bool {//查询是否在同一集合
+        int px = find(find, x);
+        int py = find(find, y);
+        if(px == py) {
+            return true;
+        } else {
+            return false;
         }
-        std::cout << '\n';
+    };
+
+    for(int i = 0; i < m; ++i) {
+        int z, x, y;
+        std::cin >> z >> x >> y;
+        x--, y--;
+        if(z == 1) {
+            uniset(x, y);
+        } else if(z == 2) {
+            std::cout << (query(x, y) ? 'Y' : 'N') << '\n';
+        }
     }
-    std::cout << "No cycles found.\n";
     return 0;
 } 
